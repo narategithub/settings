07845acda78cbda58dced5a6cbbfb0df711fd0b8
Narate Taerat <narate.github@gmail.com>
2024-03-21 13:06:52 -0500

    Update ycm_extra_conf.py

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -4,6 +4,8 @@ import json
 import socket
 import subprocess as sp
 
+# NOTE: `filename` is acutally a path.
+
 RE_SEARCH_START = re.compile(r'#include .* search starts here:')
 
 def gcc_include_search_paths():
@@ -11,7 +13,7 @@ def gcc_include_search_paths():
     paths = []
     out = sp.check_output("gcc -E -v - < /dev/null",
                           shell=True, stderr=sp.STDOUT)
-    for line in out.splitlines():
+    for line in out.decode().splitlines():
         if RE_SEARCH_START.match(line):
             search_start = 1
             continue
@@ -21,12 +23,47 @@ def gcc_include_search_paths():
         search_start = 0
     return paths
 
+def guess_project_root(filename):
+    dpath = os.path.dirname(filename)
+    dlist = dpath.split("/")
+    dpath = "/"
+    while dlist:
+        dpath += "/" + dlist[0]
+        if os.path.exists(f"{dpath}/configure.ac") or \
+           os.path.exists(f"{dpath}/README.md"):
+            return dpath
+        dpath,junk = dpath.rsplit('/', 1)
+    return None
+
+def guess_project_incdirs(proot):
+    tgt = [ proot ]
+    _pdirs = [ proot ]
+    for d in os.listdir(proot):
+        _ent = f"{proot}/{d}"
+        if os.path.isdir(_ent):
+            _pdirs.append(_ent)
+            if _ent.startswith("build"): # include build dir
+                tgt.append(_ent)
+    _guess = [ "src", "include" ]
+    for a in _pdirs:
+        for b in _guess:
+            _ent = f"{a}/{b}"
+            if os.path.isdir(_ent):
+                tgt.append(_ent)
+    return tgt
+
 def c_handle(filename):
-    h = socket.gethostname()
-    b = '/build-' + h
-    d = '/home/narate/projects/OVIS'
     flags = [ '-Wall', '-x', 'c', '-DDEBUG' ]
     flags += [ '-I' + p for p in gcc_include_search_paths() ]
+    pdir = guess_project_root(filename)
+    if pdir:
+        flags += [ '-I' + p for p in guess_project_incdirs(pdir) ]
+        path = f"{pdir}/.ycm_incdir"
+        if os.path.isfile(path):
+            f = open(path)
+            lines = f.readlines()
+            ents = [ l.strip() for l in lines ]
+            flags += [ '-I' + e for e in ents ]
     return {'flags': flags, 'do_cache': True}
 
 type_handle = {
@@ -34,14 +71,14 @@ type_handle = {
         '.h': c_handle,
         }
 
-def FlagsForFile(filename, **kwargs):
+def Settings(filename, **kwargs):
     try:
-        t = os.path.splitext(filename)
-        return type_handle[t](filename)
-    except Exception, e:
+        name, ext = os.path.splitext(filename)
+        return type_handle[ext](filename)
+    except Exception as e:
         return c_handle(filename)
 
 if __name__ == '__main__':
     # testing
-    obj = FlagsForFile("/a/b/c.h")
-    print json.dumps(obj, indent=2)
+    obj = Settings("/a/b/c.h")
+    print(json.dumps(obj, indent=2))
