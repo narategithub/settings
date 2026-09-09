da96dd7ca177145c98804b0a598123efe7001472
Narate Taerat <narategithub@gmail.com>
2026-09-09 10:20:45 -0500

    Update term-color-test.py

--- terminal.sexy/term-color-test.py
+++ terminal.sexy/term-color-test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-FG_LIST = range(30,38)
-BG_LIST = range(40,48)
+FG_LIST = [""] + [ f";{i}" for i in range(30,38) ]
+BG_LIST = [""] + [ f";{i}" for i in range(40,48) ]
 
 for bld in ["", 1]:
     for fg in FG_LIST:
         for bg in BG_LIST:
-            print(" \033[{bld};{fg};{bg}m{bld:1};{fg};{bg}\033[0m".format(**locals()), end="")
+            print(f" \033[{bld}{fg}{bg}m{bld:1}{fg}{bg}\033[0m", end="")
         print("")
         print("")
